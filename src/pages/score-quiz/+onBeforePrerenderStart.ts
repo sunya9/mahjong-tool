fcc016679c908c7b1a41ce47779d5b64d9d38925
xyz <mugen.xyz@gmail.com>
2025-12-25 22:28:48 +0900

    feat: implement onBeforePrerenderStart function to specify prerendering routes for score quiz

--- src/pages/score-quiz/+onBeforePrerenderStart.ts
+++ src/pages/score-quiz/+onBeforePrerenderStart.ts
@@ -1,11 +1,5 @@
 export function onBeforePrerenderStart() {
   return [
-    "/",
-    "/fu-quiz",
-    "/score-quiz",
-    "/fu",
-    "/glossary",
-    "/score",
     "/score-quiz/dealer",
     "/score-quiz/non-dealer",
     "/score-quiz/tsumo",
